fba3734f3aa3a54b5c7ecfc58ad2672e340af5b2
AyRidd03 <ayridd03@gmail.com>
2023-03-16 14:15:02 -0500

    Made Dwayne the Rock Johnson Come to Life Changes to be Made:     Probably setting the Circle variable changes to its own method     Images to be applied rather than circles (optional)     Ending the game when the two circles meet     Possible game title and ending flare (optional)     Cleaning up the screen refresh code (optional) Known Issues:     Circle can still go off screen

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ import math
 
 
 def main():
-    player_x = 250
-    player_y = 250
-    player_velx = 0
-    player_vely = 0
-    player_accx = 0
-    player_accy = 0
     pygame.init()
     pygame.key.set_repeat(10)  # Lets Keys be held inputs
     screen = pygame.display.set_mode((500, 500))
@@ -21,6 +15,7 @@ def main():
     s = pygame.Surface(screen.get_size(), pygame.SRCALPHA, 32)
     pygame.draw.line(s, (0, 0, 0), (250, 250), (250 + 200, 250))
 
+    p = play.player_circle(250, 250)
     width = 1
     for a_radius in range(width):
         radius = 200
@@ -28,58 +23,38 @@ def main():
 
     screen.blit(s, (0, 0))
 
-    rock_img = pygame.image.load("The_Rock.png").convert_alpha()
-    rock_img = pygame.transform.scale(rock_img, (100, 75))
-    boulder_img = pygame.image.load("Boulder.png").convert_alpha()
-    boulder_img = pygame.transform.scale(boulder_img, (100, 75))
-    player_rect = boulder_img.get_rect(center=(player_x, player_y))
-
     # Hidden Circle
     hidden_circle = pygame.draw.circle(screen, "black", (50, 100), 10, 1)
 
     pygame.display.flip()
     try:
+
         while True:
+            menu.set_cursor()
+            # menu.menu_choices(screen, s)
             event = pygame.event.wait(1)
             if event.type == pygame.QUIT:
                 break
+
             if event.type == pygame.KEYDOWN:
 
                 if event.key == pygame.K_LEFT:
-                    player_accx -= 0.5
+                    p.set_player_accx(-0.5)
                 elif event.key == pygame.K_RIGHT:
-                    player_accx += 0.5
+                    p.set_player_accx(0.5)
                 elif event.key == pygame.K_UP:
-                    player_accy -= 0.5
+                    p.set_player_accy(-0.5)
                 elif event.key == pygame.K_DOWN:
-                    player_accy += 0.5
+                    p.set_player_accy(0.5)
                 elif event.key == pygame.K_ESCAPE or event.unicode == "q":
                     break
-            # Below changes and resets values to apply physics
+            p.move_player_circle()
 
-            player_velx += player_accx
-            player_vely += player_accy
-            player_accx -= (player_accx / 8)
-            player_accy -= (player_accy / 8)
-            player_x += player_velx
-            player_y += player_vely
-            player_velx -= player_velx
-            player_vely -= player_vely
-
-            # Redraws the player's image on the screen
-            player_rect.center = (player_x, player_y)
-            screen.fill((200, 100, 50))
-            screen.blit(s, (0, 0))
-            distance = math.sqrt((player_x - 100) ** 2 + (player_y - 100) ** 2)
-
-            if distance < 50:
-                screen.blit(rock_img, player_rect)
-            else:
-                screen.blit(boulder_img, player_rect)
-
-            if hidden_circle.colliderect(player_rect):
+            if hidden_circle.colliderect(p.get_player_circle()):
                 hidden_circle = pygame.draw.circle(screen, "blue", (50, 100), 10, 1)
             pygame.display.flip()
+            p.draw_player_circle(hidden_circle, screen, s)
+
     finally:
         pygame.quit()
 
---
+++ player.py
@@ -0,0 +1,52 @@
+import pygame
+import pygame.gfxdraw
+import math
+
+
+class player_circle:
+    def __init__(self, player_x, player_y):
+        self.player_x = player_x
+        self.player_y = player_y
+        self.player_velx = 0
+        self.player_vely = 0
+        self.player_accx = 0
+        self.player_accy = 0
+        self.player_width = 200
+        self.player_height = 175
+        self.rock_img = pygame.image.load("The_Rock.png").convert_alpha()
+        self.rock_img = pygame.transform.scale(self.rock_img, (self.player_width, self.player_height))
+        self.boulder_img = pygame.image.load("Boulder.png").convert_alpha()
+        self.boulder_img = pygame.transform.scale(self.boulder_img, (self.player_width, self.player_height))
+        self.player_rect = self.boulder_img.get_rect(center=(self.player_x, self.player_y))
+        self.old_distance = 0
+
+    def move_player_circle(self):
+        self.player_velx += self.player_accx
+        self.player_vely += self.player_accy
+        self.player_accx -= (self.player_accx / 8)
+        self.player_accy -= (self.player_accy / 8)
+        self.player_x += self.player_velx
+        self.player_y += self.player_vely
+        self.player_velx -= self.player_velx
+        self.player_vely -= self.player_vely
+
+    def draw_player_circle(self, h_circle, screen, s):
+        # Redraws the player's image on the screen
+        screen.fill((200, 100, 50))
+        screen.blit(s, (0, 0))
+        self.player_rect.center = (self.player_x, self.player_y)
+        distance = math.sqrt((self.player_x - h_circle.x) ** 2 + (self.player_y - h_circle.y) ** 2)
+        if distance < self.old_distance:
+            screen.blit(self.rock_img, self.player_rect)
+        else:
+            screen.blit(self.boulder_img, self.player_rect)
+        self.old_distance = distance
+
+    def set_player_accx(self, player_accx):
+        self.player_accx += player_accx
+
+    def set_player_accy(self, player_accy):
+        self.player_accy += player_accy
+
+    def get_player_circle(self):
+        return self.player_rect
